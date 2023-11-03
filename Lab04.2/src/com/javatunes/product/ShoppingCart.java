3e6652924e7302748d8e2269ebd0a85969687335
Jelani Poole <jelani.poole@tlgcohort.com>
2023-11-03 15:40:31 -0700

    Lab04.2 (ShoppingCart<T

--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,15 +12,39 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
-  // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
-  public void addItem(T item) {
-    items.add(item);
-  }
-  
-  public void removeItem(T item) {
-    items.remove(item);
-  }
+/*
+ * At runtime, this will really be ShoppingCart<MusicItem>
+ * or ShoppingCart<MediaPlayer>,
+ * or ShoppingCart<Product>
+ */
+
+
+public class ShoppingCart<T extends Product> {
+    // storage for the cart's contents, classic HAS-A
+    private final Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+    public double total() {
+        double result = 0.0;
+        for (T item : items) {
+            result += item.getPrice();
+        }
+
+        return result;
+    }
+
+    public Collection<T> allItems() {
+        return Collections.unmodifiableCollection(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
 }
