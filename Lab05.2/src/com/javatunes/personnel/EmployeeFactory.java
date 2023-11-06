4c369c60379ab36cd05879d99fd23a9faea2da96
Jelani Poole <jelani.poole@tlgcohort.com>
2023-11-06 14:44:07 -0800

    Lab05.2 Simple Factory Employee Factory

--- Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
+++ Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
@@ -8,24 +8,48 @@
  */
 package com.javatunes.personnel;
 
+//import java.util.Date;
 import java.sql.Date;
 import java.util.Map;
 
+import static java.util.Date.*;
+
 public class EmployeeFactory {
-  
-  // prevent direct instantiation - this is an all-static factory class
-  private EmployeeFactory() {
-  }
-  
-  /**
-   * TODO: given the input map, create and return the correct object (with its properties set).
-   * If the input map's "type" value is not "HE" or "SE", throw IllegalArgumentException with a suitable message.
-   */
-  public static Employee createEmployee(Map<String,String> inputMap)
-  throws IllegalArgumentException {
-    // return value
-    Employee emp = null;
-    
-    return emp;
-  }
+
+    // prevent direct instantiation - this is an all-static factory class
+    private EmployeeFactory() {
+    }
+
+    /**
+     * given the input map, create and return the correct object (with its properties set).
+     * If the input map's "type" value is not "HE" or "SE",
+     * throw IllegalArgumentException with a suitable message.
+     */
+    public static Employee createEmployee(Map<String, String> inputMap) throws IllegalArgumentException {
+        // return value
+        Employee emp = null;
+
+        // read the 'indicator' out of the map
+        String type   = inputMap.get("type");
+
+        if (!"SE".equals(type) && !"HE".equals(type)) {
+            throw new IllegalArgumentException("Invalid type: " + type + ". Must be SE or HE.");
+        }
+
+
+        String name   = inputMap.get("name");
+        Date hireDate = Date.valueOf(inputMap.get("hireDate"));
+
+        if ("SE".equals(type)) {
+            Double salary = Double.valueOf(inputMap.get("salary"));
+            emp = new SalariedEmployee(name, hireDate, salary);
+        }
+        else {
+            Double rate = Double.valueOf(inputMap.get("rate"));
+            Double hours = Double.valueOf(inputMap.get("hours"));
+            emp = new HourlyEmployee(name, hireDate, rate, hours);
+        }
+
+        return emp;
+    }
 }
