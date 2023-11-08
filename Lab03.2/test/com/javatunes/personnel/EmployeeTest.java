1a56e56db9da19290a647ea1a0b6a1a527d613ab
Jelani Poole <jelani.poole@tlgcohort.com>
2023-11-08 12:18:07 -0800

    Employee test with anonymous subclass

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -13,9 +13,24 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp1 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
-        emp2 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        //emp1 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        //emp2 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        emp1 = new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+            public double pay() { return 0; }
+            public double payTaxes() { return 0; }
+        };
+
+        emp2 = new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+            public double pay() { return 0; }
+            public double payTaxes() { return 0; }
+        };
     }
+
+//    private Employee getEmployee() {
+//        return new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+//            public double pay() { return 0; }
+//            public double payTaxes() { return 0; }
+//    };
 
     @Test
     public void equals_shouldReturnFalse_differentName_sameHireDate() {
@@ -32,5 +47,16 @@ public class EmployeeTest {
     @Test
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(emp1, emp2);
+    }
+
+    // NAMED, MEMBER LEVEL INNER CLASSES
+    private class DummyEmployee extends Employee {
+
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        public double pay() { return 0; }
+        public double payTaxes() { return 0; }
     }
 }
